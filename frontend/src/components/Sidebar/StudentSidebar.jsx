7850ce24f843de6edaf16ab8e9df55502bf8ef4b
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2025-07-24 18:03:46 +0530

    student sidebar added

--- frontend/src/components/Sidebar/StudentSidebar.jsx
+++ frontend/src/components/Sidebar/StudentSidebar.jsx
@@ -6,39 +6,124 @@ function StudentSidebar() {
   const location = useLocation();
 
   return (
-    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-      <div className="d-flex">
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        width: "100%",
+      }}
+    >
+      <div class="d-flex align-items-start w-100">
         <div
-          className="bg-white text-black p-3"
-          style={{width: "250px", minHeight: "100vh"}}
+          class="nav flex-column nav-pills me-3 w-100"
+          id="v-pills-tab"
+          role="tablist"
+          aria-orientation="vertical"
         >
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <a className="nav-link text-black" href="#">
-                Dashboard
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-black" href="#">
-                Profile
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-black" href="#">
-                Course
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-black" href="#">
-                Attendance
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-black" href="#">
-                Fees
-              </a>
-            </li>
-          </ul>
+          <button
+            className="nav-link active w-100"
+            id="v-pills-dashboard-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-dashboard"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-dashboard"
+            aria-selected="true"
+            onClick={() => navigate("/student/dashboard")}
+          >
+            Dashboard
+          </button>
+          <button
+            className="nav-link w-100"
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-home"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="false"
+            onClick={() => navigate("/student/profile")}
+          >
+            Profile
+          </button>
+          <button
+            class="nav-link w-100"
+            id="v-pills-profile-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-profile"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-profile"
+            aria-selected="false"
+            onClick={() => navigate("/student/attendance")}
+          >
+            Attendance
+          </button>
+
+          <button
+            class="nav-link w-100"
+            id="v-pills-messages-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-messages"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-messages"
+            aria-selected="false"
+          >
+            Messages
+          </button>
+          <button
+            class="nav-link w-100"
+            id="v-pills-settings-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-settings"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-settings"
+            aria-selected="false"
+            onClick={() => navigate("/student/fee")}
+          >
+            Fee
+          </button>
+        </div>
+        <div class="tab-content" id="v-pills-tabContent">
+          <div
+            class="tab-pane fade show active"
+            id="v-pills-dashboard"
+            role="tabpanel"
+            aria-labelledby="v-pills-dashboard-tab"
+            tabindex="0"
+          ></div>
+          <div
+            class="tab-pane fade"
+            id="v-pills-home"
+            role="tabpanel"
+            aria-labelledby="v-pills-home-tab"
+            tabindex="0"
+          ></div>
+          <div
+            class="tab-pane fade"
+            id="v-pills-profile"
+            role="tabpanel"
+            aria-labelledby="v-pills-profile-tab"
+            tabindex="0"
+          ></div>
+          <div
+            class="tab-pane fade"
+            id="v-pills-messages"
+            role="tabpanel"
+            aria-labelledby="v-pills-messages-tab"
+            tabindex="0"
+          ></div>
+          <div
+            class="tab-pane fade"
+            id="v-pills-settings"
+            role="tabpanel"
+            aria-labelledby="v-pills-settings-tab"
+            tabindex="0"
+          ></div>
         </div>
       </div>
     </div>
