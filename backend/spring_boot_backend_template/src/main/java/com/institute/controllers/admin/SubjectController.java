047db7ac2a0a52f453f83d869d47af9276d1c4d4
Vedant Choudhari <ved10.choudhari@gmail.com>
2025-07-27 17:01:07 +0530

    subjects api completed and some changes in mapping is done as added new coursesubjectteacher mapping

--- backend/spring_boot_backend_template/src/main/java/com/institute/controllers/admin/SubjectController.java
+++ backend/spring_boot_backend_template/src/main/java/com/institute/controllers/admin/SubjectController.java
@@ -6,13 +6,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.institute.dto.ApiResponse;
 import com.institute.dto.SubjectDto;
 import com.institute.service.admin.SubjectService;
 
@@ -45,7 +49,19 @@ public class SubjectController {
 	                         .body(subjectService.addSubject(subjectDto));
 	}
 	
-	
+	@PutMapping("/edit-subject/{subjectId}")
+	@Operation(summary = "Update subject", description = "Updates the details of a subject by its ID.")
+	public ResponseEntity<?> editSubjectByID(
+	        @PathVariable Long subjectId,
+	        @Valid @RequestBody SubjectDto dto) {
+	    return ResponseEntity.ok(subjectService.updateSubjectsById(subjectId, dto));
+	}
 
-	
+	@DeleteMapping("/{subjectId}")
+	@Operation(summary = "Soft delete subject", description = "Soft deletes the subject if its status is INACTIVE.")
+	public ResponseEntity<?> deleteSubject(@PathVariable Long subjectId) {
+	    return ResponseEntity.ok(subjectService.deleteSubjectsById(subjectId));
+	}
+
+		
 }
