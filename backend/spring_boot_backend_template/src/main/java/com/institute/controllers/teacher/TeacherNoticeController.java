165fba3024ce9d0c28d0b96437f27935b5853e9f
Sahil Baranga <sahilbaranga07@gmail.com>
2025-08-06 11:52:32 +0530

    fixed small bug

--- backend/spring_boot_backend_template/src/main/java/com/institute/controllers/teacher/TeacherNoticeController.java
+++ backend/spring_boot_backend_template/src/main/java/com/institute/controllers/teacher/TeacherNoticeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.institute.service.admin.NoticeService;
-<
+
 import com.institute.service.teacher.TeacherNoticeService;
 
 import io.swagger.v3.oas.annotations.Operation;
