99046205966df56f676bcf0e35c00a80384d25df
Vedant Choudhari <ved10.choudhari@gmail.com>
2025-08-06 15:27:44 +0530

    student and fee apis of admin modified

--- backend/spring_boot_backend_template/src/main/java/com/institute/controllers/student/StudentEntityController.java
+++ backend/spring_boot_backend_template/src/main/java/com/institute/controllers/student/StudentEntityController.java
@@ -1,21 +1,29 @@
 package com.institute.controllers.student;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.institute.dto.student.StudentAttendanceDto;
 import com.institute.dto.student.StudentFeeDto;
 import com.institute.dto.student.StudentProfileDto;
 import com.institute.dto.student.UpdateStudentProfileDto;
-import com.institute.security.AuthUtil;
-import com.institute.service.student.*;
+import com.institute.service.student.StudentAttendanceService;
+import com.institute.service.student.StudentEntityService;
+import com.institute.service.student.StudentFeeService;
+import com.institute.service.student.StudentMarksService;
+import com.institute.service.student.StudentProfileService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-
-import java.util.Optional;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @SecurityRequirement(name = "bearerAuth")
 @RestController
